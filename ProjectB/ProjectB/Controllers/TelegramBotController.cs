a448abb55db01414e26fbf77678a110e6b38972f
vesko <v.metodiev1718@gmail.com>
2021-08-31 12:13:44 +0300

    Added Telegram Bot Integration

---
+++ ProjectB/ProjectB/Controllers/TelegramBotController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ProjectB.Controllers
+{
+    [ApiController]
+    [Route("api/bot")] //https:port/api/bot
+    public class TelegramBotController : ControllerBase
+    {
+
+        //ToDo: Add the Dialog flow with the BOT
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Update update) // FromBody means get update from body section of HTTP request
+        {                                                   // Update update is what we receive from Telegram
+
+            TelegramBotClient client = new TelegramBotClient("BotTokenGoesHere");
+
+            // When someone sends message to Bot, Telegram sends update object to url set in the webhook.
+            //Just send message with text "answer" to the same chat(Update.Message.From.Id)
+            if(update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                await client.SendTextMessageAsync(update.Message.From.Id, "answer");
+            }
+
+            return Ok();
+        }
+    }
+}
--- ProjectB/ProjectB/Startup.cs
+++ ProjectB/ProjectB/Startup.cs
@@ -30,8 +30,7 @@ namespace ProjectB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(); // need to correctly parse Data coming from Telegram
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProjectB", Version = "v1" });
